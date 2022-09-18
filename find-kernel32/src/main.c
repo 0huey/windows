b040c5bfc92d7896452eda9155d8a66153b21d2e
0huey <34045167+0huey@users.noreply.github.com>
2022-09-17 18:47:43 -0700

    display target process info

--- find-kernel32/src/main.c
+++ find-kernel32/src/main.c
@@ -3,7 +3,21 @@
 __declspec( dllexport ) VOID DllRegisterServer(VOID) {
     FindKernel32();
 
-    fMessageBoxA = LocateFunction("user32.dll", "MessageBoxA");
+    PGET_CURRENT_PROCESS_ID GetCurrentProcessId = LocateFunction("kernel32.dll", "GetCurrentProcessId");
+    PGET_CURRENT_PROCESS    GetCurrentProcess   = LocateFunction("kernel32.dll", "GetCurrentProcess");
+    PGET_MODULE_BASE_NAME_A GetModuleBaseNameA  = LocateFunction("psapi.dll", "GetModuleBaseNameA");
+    PMESSAGE_BOX_A          MessageBoxA         = LocateFunction("user32.dll", "MessageBoxA");
+    PS_PRINTF_S             sprintf_s           = LocateFunction("msvcrt.dll", "sprintf_s");
+
+    HANDLE hProcess = GetCurrentProcess();
+
+    CHAR procName[PROC_NAME_LEN];
+
+    GetModuleBaseNameA(hProcess, NULL, procName, PROC_NAME_LEN);
+
+    CHAR msg[MSG_LEN];
     
-    fMessageBoxA(0, "test", "test", MB_OK);
+    sprintf_s(msg, MSG_LEN, "Running from %s PID %ld", procName, GetCurrentProcessId());
+
+    MessageBoxA(0, msg, "Injected", MB_OK);
 }
--- find-kernel32/src/main.h
+++ find-kernel32/src/main.h
@@ -1,8 +1,20 @@
 #include <windows.h>
 #include "find-kernel32.h"
 
-typedef int MESSAGE_BOX_A(HWND, LPCSTR, LPCSTR, UINT);
+#define MSG_LEN 256
+#define PROC_NAME_LEN 128
 
+typedef DWORD GET_CURRENT_PROCESS_ID();
+typedef GET_CURRENT_PROCESS_ID *PGET_CURRENT_PROCESS_ID;
+
+typedef HANDLE GET_CURRENT_PROCESS();
+typedef GET_CURRENT_PROCESS *PGET_CURRENT_PROCESS;
+
+typedef DWORD GET_MODULE_BASE_NAME_A(HANDLE, HMODULE, LPSTR, DWORD);
+typedef GET_MODULE_BASE_NAME_A *PGET_MODULE_BASE_NAME_A;
+
+typedef int MESSAGE_BOX_A(HWND, LPCSTR, LPCSTR, UINT);
 typedef MESSAGE_BOX_A *PMESSAGE_BOX_A;
 
-PMESSAGE_BOX_A fMessageBoxA = NULL;
+typedef int S_PRINTF_S(char *, size_t, const char *, ...);
+typedef S_PRINTF_S *PS_PRINTF_S;
